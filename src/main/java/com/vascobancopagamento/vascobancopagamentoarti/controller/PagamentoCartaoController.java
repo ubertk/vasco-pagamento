5d7244575b6abab5822077b0e7c8755d78c29875
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2023-06-10 17:43:29 -0300

    iniciando pagamento cartao

---
+++ src/main/java/com/vascobancopagamento/vascobancopagamentoarti/controller/PagamentoCartaoController.java
@@ -0,0 +1,10 @@
+package com.vascobancopagamento.vascobancopagamentoarti.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/pagCartao")
+public class PagamentoCartaoController {
+    
+}
